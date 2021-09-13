aba4962799fe681c274cbf1cd0ba4ba0ee955327
Artyom Lazyan <42096630+LazyanArtyom@users.noreply.github.com>
2021-09-13 20:34:52 +0400

    last try :D

--- Files_via_cout/main.cpp
+++ Files_via_cout/main.cpp
@@ -2,39 +2,15 @@
 #include <unistd.h>
 #include <iostream>
 
-// witouth using dup2() function
-void redirect(const char* fname)
-{
-    int no = open(fname, O_RDWR|O_CREAT|O_APPEND, 0600);
-    stdout->_fileno = no;
-}
-
-void redirect_safe(const char* fname)
-{
-    int out = open(fname, O_RDWR|O_CREAT|O_APPEND, 0600);
-    if (!out)
-    { 
-        perror("error opening out.txt");
-        exit(-1); 
-    }
-    
-    // Reassigning a File Descriptor
-    // dup2() to duplicate a file descriptor, 
-    // which will allow us to redirect output
-    // STDOUT_FILENO is a file desriptor of stdout 
-    if (!dup2(out, STDOUT_FILENO))
-    {
-        perror("cannot redirect stdout"); 
-        exit(-1); 
-    }
-
-    close(out);
-}
-
 int main()
 {
-    redirect("output.txt");
-    std::cout << "Hello world\n" << std::endl;
+    // As we know that the file descriptor returned by
+    // a successful call will be the lowest-numbered file descriptor not
+    // currently open for the process. :D
+    // and 1 is a file descriptor of stdout
+    close(1);
+    int fd = open("output.txt", O_RDWR|O_CREAT|O_APPEND, 0600);
+    std::cout << fd << "Hello world\n" << std::endl;
 
     return 0;
 }
